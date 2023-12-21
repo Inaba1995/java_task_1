c455c51f2794cbdffeb1d18a876a9b69615b18cb
Nikita Semenenko <Nik21081995@yandex.ru>
2023-12-21 21:13:25 +0300

    удаление сканера

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        // объявляем переменную типа Scanner для считывания с консоли ввода пользователя
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Введите стоимость билета: ");
-        int price = sc.nextInt(); // считываем ввод пользователя
+        int price = 12000;
         // количество рублей, за которое начисляют 1 бонус/милю
         final int countRub = 20;
         // рассчитываем количество бонусов при помощи целочисленного деления
